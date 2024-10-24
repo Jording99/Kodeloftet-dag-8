bcc5d72ec268f5b6832eabf4df2d24427d5225f1
Jording99 <pederjo@hotmail.no>
2024-10-24 13:22:15 +0200

    JavaScript

--- script.js
+++ script.js
@@ -4,15 +4,28 @@ console.log('Hello World!');
 
 //Let variables saving the number 28 on the age name
 let age = 25;
-console.log(age);
-console.log(typeof age)
+//Charning mathod:
+console.log("Min alder er:", age);
+//Tamplate litteral
+console.log(`Min alder er ${age}`);
+console.log(typeof age);
 
 //Const variables saves the string Julie on name name.
 const name = "Jørgen";
-console.log(name)
-console.log(typeof name)
+console.log("Mitt navn er:", name);
+console.log(typeof name);
 
 // Const variables saving the boolean true on the name 
 const isPresent = true
-console.log(isPresent)
-    console.log(typeof isPresent)
+console.log("Er tilstede?",isPresent);
+console.log(typeof isPresent);
+
+//Two const variables saving strings
+//One const variable saving the data from the two first variables
+const firstName = "Jørgen";
+const lastName = "Ording";
+const fullName = firstName+" "+lastName;
+console.log(fullName, "er navnet mitt");
+
+const htmlElement=document.querySelector("#javaScriptData")
+htmlElement.textcontent=fullName;
