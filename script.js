57b86c93b5468d6a46ea4b14b13db2fa307e7997
Jording99 <pederjo@hotmail.no>
2024-10-24 10:51:42 +0200

    Added info

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello!');
+console.log('Hello World!');
+
+//Variables
+
+//Let variables saving the number 28 on the age name
+let age = 25;
+console.log(age);
+console.log(typeof age)
+
+//Const variables saves the string Julie on name name.
+const name = "Jørgen";
+console.log(name)
+console.log(typeof name)
+
+// Const variables saving the boolean true on the name 
+const isPresent = true
+console.log(isPresent)
+    console.log(typeof isPresent)
